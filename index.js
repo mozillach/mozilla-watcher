60f156bac01808e47673b1771e147297aa9a8882
Michael Kohler <mkohler@picobudget.com>
2016-10-20 01:52:07 +0200

    Move fetchPagesRecursively to it's own method on the helper

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const debug = require('debug')('index');
 const GitHubHelper = require('./lib/github-helper');
 const RedisHandler = require('./lib/redis-handler');
 
-const githubHelper = new GitHubHelper();
-const redisHandler = new RedisHandler();
-
 const REPO_NAME = 'mozillach';
+const githubHelper = new GitHubHelper(REPO_NAME);
+const redisHandler = new RedisHandler();
 
 redisHandler.getLastCheckDate()
 .then((lastCheckDate) => {
-  return githubHelper.getNewRepos(REPO_NAME, lastCheckDate);
+  return githubHelper.getNewRepos(lastCheckDate);
 })
 .then((newRepositories) => {
   debug(`got ${newRepositories.length} new repositories`);
