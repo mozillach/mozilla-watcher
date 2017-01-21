e63a4c25e365261b5ea27cf4f864ac01f141dde4
Michael Kohler <me@michaelkohler.info>
2017-01-21 18:47:27 +0100

    Sort newest repos

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,9 @@ const WIKI_KEY = 'WIKI_EDITS';
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  const repos = storageHandler.getStorageItem(REPO_KEY).slice(0, 100);
+  const repos = storageHandler.getStorageItem(REPO_KEY).sort((a, b) => {
+    return new Date(b.created_at) - new Date(a.created_at);
+  }).slice(0, 100);;
 
   const updatedRepos = storageHandler.getStorageItem(REPO_KEY).sort((a, b) => {
     return new Date(b.updated_at) - new Date(a.updated_at);
