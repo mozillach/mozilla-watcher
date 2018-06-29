b32cc5808b3d170ab1bf3f94bc470d7eba669c41
Michael Kohler <me@michaelkohler.info>
2018-06-29 20:26:14 +0200

    Refactor

--- app.js
+++ app.js
@@ -9,12 +9,9 @@ const index = require('./routes/index');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,20 +20,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- fetch.js
+++ fetch.js
@@ -9,29 +9,32 @@ const REPO_NAMES = require('./organizations.json');
 
 const githubAuth = {
   username: args.username,
-  token: args.token
+  token: args.token,
 };
 
-const REPO_NAME = 'mozillach';
 const githubHelper = new GitHubHelper(REPO_NAMES, githubAuth);
 const storageHandler = new StorageHandler();
 const wikiHelper = new WikiHelper();
 
+debug('Starting everything...');
+fetchAll();
+
 async function fetchAll() {
-  let lastCheckDate = await storageHandler.getLastRepoCheckDate();
-  let newRepositories = await githubHelper.getAll(lastCheckDate);
+  const lastCheckDate = await storageHandler.getLastRepoCheckDate();
+
+  const [newRepositories, newestEdits] = await Promise.all([
+    githubHelper.getAll(lastCheckDate),
+    wikiHelper.getLatestEdits(),
+  ]);
+
   debug(`got ${newRepositories.length} new repositories`);
+  debug(`got ${newestEdits.length} wiki edits`);
 
-  let latestRunDate = githubHelper.getLatestRunStartDate();
-  let difference = await storageHandler.saveDifference(latestRunDate, newRepositories);
+  const latestRunDate = githubHelper.getLatestRunStartDate();
+  const difference = await storageHandler.saveDifference(latestRunDate, newRepositories);
 
-  // TODO: this can run in parallel
-  let newestEdits = await wikiHelper.getLatestEdits();
-  let lastEditRunDate = wikiHelper.getLastWikiCheckDate();
-  await storageHandler.saveWikiEdits(lastEditRunDate, newestEdits);
+  const lastWikiRunDate = wikiHelper.getLastWikiCheckDate();
+  await storageHandler.saveWikiEdits(lastWikiRunDate, newestEdits);
 
   debug('All done!');
 }
-
-debug('Starting everything...');
-fetchAll();
