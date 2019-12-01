d7254c572c751abd772f9d5136c0544983579f09
Michael Kohler <me@michaelkohler.info>
2019-12-01 22:44:33 +0100

    Use MySQL instead of redis

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const StorageHandler = require('../lib/storage-handler');
 const router = express.Router();
-const storageHandler = new StorageHandler();
-
-const REPO_KEY = 'REPOSITORIES';
-const WIKI_KEY = 'WIKI_EDITS';
+const storageHandler = StorageHandler.getInstance();
 
 router.get('/', async (req, res, next) => {
-  const repos = await storageHandler.getStorageItem(REPO_KEY);
-  const wikiEdits = await storageHandler.getStorageItem(WIKI_KEY);
+  const repos = await storageHandler.getRepos();
+  const wikiEdits = await storageHandler.getWikiEdits();
 
   res.render('index', {
     repos,
